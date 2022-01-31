53cac2794b9b3e3550f9461b05825516c898c3ec
unknown <jk04032002@gmail.com>
2022-02-01 00:45:21 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/teamchata/charta/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.teamchata.charta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: const MyHomePage(title: 'Charta'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title,style:TextStyle(color: Colors.white,fontSize:30,fontWeight: FontWeight.bold),),
+      ),
+      body: Center(
+
+        child: Container(decoration:BoxDecoration(color: Colors.green,borderRadius:BorderRadius.all(Radius.circular(10))),height:100,width: 300,child: Center(child:Text("Charta",style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold,fontSize: 90),)),)
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
